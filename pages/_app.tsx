9f5ca819749460f69ba2855256ee1bc3646bbee5
Rafael Dias <rafaelldiias100@gmail.com>
2022-12-03 20:49:21 -0300

    feat: criação da tela Home e instalando fonte Inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
 import Header from "../components/Header";
+
+const inter = Inter({
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={`${inter.variable} font-inter`}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>CryptoNext</h1>
+      <main className="h-screen flex items-center p-36 bg-gray-800">
+        <section>
+          <h1 className="font-bold text-4xl text-white">CryptoNext</h1>
+          <p className="text-2xl text-white">
+            Tudo sobre <span className="text-orange-400">criptomoedas</span> num
+            só lugar!
+          </p>
+        </section>
       </main>
     </div>
   );
